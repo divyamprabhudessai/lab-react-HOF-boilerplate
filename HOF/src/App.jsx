08d90797d5d5723c3228b07bb6bfc4dda4e86de0
divyamprabhudessai <divyam.desai@kalvium.community>
2024-01-08 18:07:21 +0530

    final code

---
+++ HOF/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HigherOrderComponent from './components/HigherOrderComponent'
+
+function App() {
+
+  return (
+    <div className='App'>
+
+      <HigherOrderComponent/>
+
+    </div>
+
+  )
+}
+
+export default App
